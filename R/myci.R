2f00c870ed03940e5bee99047b37d05b2d520313
Mark Desktop <marklouiscastle@gmail.com>
2024-11-08 17:16:01 -0600

    added myci to package

---
+++ R/myci.R
@@ -0,0 +1,19 @@
+#' Confidence interval for the mean
+#'
+#' @param x a vector of quantitative data
+#'
+#' @return a vector of length 2 with the lower and upper bounds of the confidence interval
+#' @export
+#'
+#' @examples
+#' myci(x=1:10)
+myci <- function(x){
+  n <- length(x)
+  mean_x <- mean(x)
+  sd_x <- stats::sd(x)
+
+  alpha <- 0.05
+  t_alpha_2 <- stats::qt(1-alpha/2, df=n-1)
+
+  CI <- mean_x + c(-1,1)*t_alpha_2*sd_x/sqrt(n)
+}
